c90f7bb7b32915a5f5c93bdcb383d84869da3033
harryfittheorem <82176224+harryfittheorem@users.noreply.github.com>
2026-01-07 12:01:28 -0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
@@ -8,6 +9,19 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about-page.html'),
+        findLocation: resolve(__dirname, 'find-your-location.html'),
+        formSubmit: resolve(__dirname, 'form-submit-page.html'),
+        franchise: resolve(__dirname, 'franchise-page.html'),
+        groupFitness: resolve(__dirname, 'group-fitness.html'),
+        kickboxing: resolve(__dirname, 'kickboxing.html'),
+        schedule: resolve(__dirname, 'schedule-page.html'),
+        thankYou: resolve(__dirname, 'thank-you-page.html')
+      }
+    }
   }
 });
